3cec28517808201240a1e767643fd46f58b2085a
jezus <90801079+solnMarket@users.noreply.github.com>
2024-06-25 08:24:33 +0800

    finished the project
    created 3 buttons and a div
    replicated the rock,paper,scissors game functionalities
    display who won

--- dynamic.js
+++ dynamic.js
@@ -23,67 +23,112 @@ function getHumanChoice(){
     return input;
 }
 
+function playRound(human){
+    const d = document.querySelector(".score")
+    c=getComputerChoice();
 
+    if(human == 'ROCK'){
+        if(c=='Rock'){
+            d.textContent='its a tie'
+        }else if(c=='Paper'){
+            d.textContent="YOU LOSE"
+            return true;
+        }else{
+            d.textContent="YOU WIN"
+            return false;
+        }
+    }
+    else if(human == 'PAPER'){
+        if(c=='Rock'){
+            d.textContent="YOU WIN!"
+            return true;
+            
+        }else if(c=='Paper'){
+             d.textContent="its a tie"
+             
+        }else{
+            d.textContent="you just lost to a machine dumbass, you dont deserve the title of human, you're the reason descartes believes in dualism."
+            return false;
+        }
+
+    }
+    else if(human== 'SCISSORS'){
+        if(c=='Rock'){
+            d.textContent="you lost goblin, thats what i say when i refer to you."
+             return false;
+             
+        }else if(c=='Paper'){
+            d.textContent="you won finally whooo"
+            return true;
+            
+        }else{
+            d.textContent="its a tie, goblin"
+            
+        }
+    }
+}
 
 function playGame(){
     let ComputerScore=0;
     let humanScore=0;
 
-    
-    function playRound(){
-        h=getHumanChoice();
-        c=getComputerChoice();
-        human=h.toUpperCase();
-    
-        if(human == 'ROCK'){
-            if(c=='Rock'){
-                console.log("tie");
-            }else if(c=='Paper'){
-                ComputerScore++;
-                console.log("you lose");
-            }else{
-                humanScore++;
-                 console.log("you win");
-            }
-        }
-        else if(human == 'PAPER'){
-            if(c=='Rock'){
-                humanScore++;
-                console.log("you win");
-                
-            }else if(c=='Paper'){
-                 console.log("tie");
-                 
-            }else{
-                ComputerScore++;
-                 console.log("you lose");
-                 
-            }
-    
-        }
-        else if(human== 'SCISSORS'){
-            if(c=='Rock'){
-                ComputerScore++;
-                 console.log("you lose");
-                 
-            }else if(c=='Paper'){
-                humanScore++;
-                console.log("you win");
-                
-            }else{
-                console.log("its a tie!!!"); 
-                
-            }
-        }
-    }
-    
-    for(let i=0;i<5;i++){
-        playRound();
-    }
-    if(humanScore>ComputerScore){
-        console.log("human wins")
-    }else{
-        console.log("Computer wins!")
-    }
+    playRound()
 }
+
+const r = document.querySelector(".rock")
+const p = document.querySelector(".paper")
+const s = document.querySelector(".scissors")
+
+
+let compScore=0
+let humScore=0
+
+r.addEventListener("click", function(){
+    let rock ="ROCK"
+    let winner = playRound(rock) 
+    if(winner==1){
+        humScore++;
+    }
+    else{
+        compScore++
+    }
+    checkWinner()
+})
+ p.addEventListener("click",function(d){
+    let paper ="PAPER"
+    let winner = playRound(paper,d) 
+    if(winner==1){
+        humScore++;
+    }
+    else{
+        compScore++
+    }
+    checkWinner()
+ })
+ s.addEventListener("click",function(d){
+    let scissors ="SCISSORS "
+    let winner = playRound(scissors,d) 
+    if(winner==1){
+        humScore++;
+    }
+    else{
+        compScore++
+    }
+    checkWinner()
+ })
+ const screen = document.querySelector(".screen")
+ const newt = document.createElement("div")
+ screen.appendChild(newt)
+
+ function checkWinner(){
+    if(humScore==5){
+        newt.textContent="you won";
+        
+     }
+     if(compScore==5){
+        newt.textContent="you lost"
+     }
+ }
+
+
 
